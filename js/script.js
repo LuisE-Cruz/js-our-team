f80ba1fc2f242149db884b7e21a3eb6f95ae2515
LuisE Cruz <Luis.cruzlc01@gmail.com>
2025-12-16 18:11:48 +0100

    Creata funzione con dentro l'html delle card e gli object dell'array dato  e funzione che cicla l'array object e inietta il tutto dentro l'html

--- js/script.js
+++ js/script.js
@@ -37,4 +37,77 @@ const teamMembers = [
   }
 ];
 
+// SETUP
 const teamContainer = document.querySelector(".container-team");
+
+
+rederTeam(teamMembers, teamContainer)
+
+
+
+
+
+
+
+// FUNZIONI
+
+// Funzione che genera la card e prende le info dall'array
+function createTeamCard(obj) {
+  const card = `
+            <div class="col-12 col-md-4 ">
+
+                <div class="bg-dark d-flex">
+
+                    <img src="${obj.img}" alt="${obj.name}">
+
+                    <div class="d-flex flex-column align-items-start">
+
+                        <h3 class="text-light my-3 ms-2">${obj.name}</h3>
+                        <p class="text-light my-3 ms-2">${obj.role}</p>
+                        <a class="my-3 ms-2" href="#">${obj.email}</a>
+
+                    </div>
+
+                </div>
+
+            </div>
+  `;
+  return card;
+}
+
+//Funzione che serve a fare il rendering delle diverse card dentro l'html grazie ad un ciclo for
+
+// function showCards(array, outputEl){
+
+//   let cards = '';
+
+//   for (let index = 0; index < array.length; index++) {
+
+//     const member = array[index];
+
+//     cards += createTeamCard(member);
+//   }
+
+// }
+
+
+
+function rederTeam(arraRef, outputEL) {
+    // mi creo var che accumulerà gli elementi di ouput
+    let cards = "";
+
+    // output di prova ciclando l'array di oggetti
+    for (let index = 0; index < arraRef.length; index++) {
+        // estrapoliamo ogni volta un oggetto membro divero
+        const memberTeam = arraRef[index];
+
+        // destrutturiamo l'oggetto
+        // const { name, image, role } = memberTeam;
+
+        // andiamo ad ogni giro ad incrementare il contenuto dell'output
+        cards += createTeamCard(memberTeam);
+    }
+
+    // inseriamo la stringa completa che innerHTML trasformerà, nell'elemento di output
+    outputEL.innerHTML = cards;
+}
